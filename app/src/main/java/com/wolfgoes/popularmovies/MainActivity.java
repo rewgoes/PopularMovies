6640f686ef41ebf9d278e2a180cb61997eea92a4
Rafael Wolf de Goes <rewgoes@gmail.com>
2016-07-19 23:44:56 -0300

    Add GridView

--- app/src/main/java/com/wolfgoes/popularmovies/MainActivity.java
+++ app/src/main/java/com/wolfgoes/popularmovies/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final String LOG_TAG = MainActivity.class.getSimpleName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
