a7cbe856bebaf2bcc1bca11cdaa7884e22546ee8
Rafael Wolf de Goes <rewgoes@gmail.com>
2016-08-08 21:04:25 -0300

    Create DetailFragment and change app's icon

--- app/src/main/java/com/wolfgoes/popularmovies/ui/DetailActivity.java
+++ app/src/main/java/com/wolfgoes/popularmovies/ui/DetailActivity.java
@@ -1,8 +1,18 @@
 package com.wolfgoes.popularmovies.ui;
 
 import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ShareActionProvider;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.wolfgoes.popularmovies.R;
@@ -13,17 +23,102 @@ public class DetailActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detail_acitivy);
+        setContentView(R.layout.activity_detail);
+        setContentView(R.layout.activity_detail);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new DetailFragment())
+                    .commit();
+        }
+    }
 
-        TextView text = (TextView) findViewById(R.id.texting);
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.detail, menu);
+        return true;
+    }
 
-        Intent intent = getIntent();
-        if (intent != null) {
-            if (intent.hasExtra("movie")) {
-                Movie movie = intent.getParcelableExtra("movie");
-                if (movie != null )
-                    text.setText(movie.getTitle());
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            startActivity(new Intent(this, SettingsActivity.class));
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class DetailFragment extends Fragment {
+
+        private static final String LOG_TAG = DetailFragment.class.getSimpleName();
+
+        private static final String MOVIE_SHARE_HASHTAG = " #PopularMovies";
+        private String mMovieStr;
+
+        public DetailFragment() {
+            setHasOptionsMenu(true);
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+
+            View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
+
+            TextView text = (TextView) rootView.findViewById(R.id.texting);
+
+            Intent intent = getActivity().getIntent();
+            if (intent != null) {
+                if (intent.hasExtra("movie")) {
+                    Movie movie = intent.getParcelableExtra("movie");
+                    if (movie != null ) {
+                        text.setText(movie.getTitle());
+                        mMovieStr = movie.getTitle();
+                    }
+                }
             }
+
+            return rootView;
+        }
+
+        @Override
+        public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+            // Inflate the menu; this adds items to the action bar if it is present.
+            inflater.inflate(R.menu.detailfragment, menu);
+
+            // Retrieve the share menu item
+            MenuItem menuItem = menu.findItem(R.id.action_share);
+
+            // Get the provider and hold onto it to set/change the share intent.
+            ShareActionProvider mShareActionProvider =
+                    (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
+
+            // Attach an intent to this ShareActionProvider.  You can update this at any time,
+            // like when the user selects a new piece of data they might like to share.
+            if (mShareActionProvider != null ) {
+                mShareActionProvider.setShareIntent(createShareForecastIntent());
+            } else {
+                Log.d(LOG_TAG, "Share Action Provider is null?");
+            }
+        }
+
+        private Intent createShareForecastIntent() {
+            Intent shareIntent = new Intent(Intent.ACTION_SEND);
+            shareIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+            shareIntent.setType("text/plain");
+            shareIntent.putExtra(Intent.EXTRA_TEXT,
+                    mMovieStr + MOVIE_SHARE_HASHTAG);
+            return shareIntent;
         }
     }
 }
